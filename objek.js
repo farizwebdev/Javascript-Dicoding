f1629c0f5161a8cffa1584afc1c9243fc499dc88
farizwebdev <farizabar80@gmail.com>
2025-12-15 09:42:14 +0700

    ubah di kuis

--- objek.js
+++ objek.js
@@ -1,11 +1,12 @@
+// Buat objek 
 const saya = {
-    nama: "Fariiz",
+    nama: "Fariz",
     hobi: "Jogging",
     umur: 19,
 }
 
+// Akses properti objek nama
 console.log(saya.nama);
 
 delete saya.hobi;
 console.log(saya);
-
