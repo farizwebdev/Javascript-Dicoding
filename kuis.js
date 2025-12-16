0c60729c123ec7ba7949d39b28f8e8951bdd8760
farizwebdev <farizabar80@gmail.com>
2025-12-16 23:34:22 +0700

    edit kuis

--- kuis.js
+++ kuis.js
@@ -11,3 +11,4 @@ function addEmployee(name, email, JoinYear) {
 // Menampilkan Hasil 
 console.log(addEmployee('Fariz', 'fariz@gmail.com', 2025));
 console.log(addEmployee('Abdurrahman', 'abdur@gmail.com', 2024));
+console.log(addEmployee('Muhammad', 'muhammad@gmail.com', 2023));
