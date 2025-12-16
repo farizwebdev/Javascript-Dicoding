56860cc0a5ecf5a01a5cad8a864788f8e9c4e4ea
farizwebdev <farizabar80@gmail.com>
2025-12-16 23:35:50 +0700

    edit objek

--- objek.js
+++ objek.js
@@ -11,5 +11,8 @@ console.log(saya.nama);
 // Kalo mo ngehapus properti Hobi
 delete saya.hobi;
 
+// Kalo mo nambah properti pekerjaan
+saya.pekerjaan = "Mahasiswa";
+
 // Kalo mo nampilin objek setelah dihapus properti hobinya
 console.log(saya);
