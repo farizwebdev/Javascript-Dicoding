0ca8a780e3a2b374f66d8655f13dc0a9f3c2b5f7
farizwebdev <farizabar80@gmail.com>
2025-12-17 17:10:16 +0700

    edit

---
+++ if_statement.js
@@ -0,0 +1,9 @@
+const anak_yuin = true;
+
+console.log("Yeay, aku anak Yuin!")
+
+if (anak_yuin) {
+    console.log("Sanaa, sholattt duluu!");
+}
+
+console.log("Aku anak Yuin kok bukan anak Budi");
