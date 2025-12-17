7f57d4fe533f331f2399733c7f99c44f87a5378f
farizwebdev <farizabar80@gmail.com>
2025-12-18 03:45:17 +0700

    ubah

--- while.js
+++ while.js
@@ -3,3 +3,11 @@ while (uang < 5) {
     console.log(`Uang saya sekarang berjumlah ${uang} triliun rupiah`);
     uang++;
 };
+
+// do while
+let ayam_kampus = 0;
+
+do {
+    console.log(`Ayam kampus berjumlah ${ayam_kampus} ekor`);
+    ayam_kampus++;
+} while (ayam_kampus < 5);
