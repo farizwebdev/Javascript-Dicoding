03f3029c47dbd31c62a14ab6ba420c3e762e3e38
farizwebdev <farizabar80@gmail.com>
2025-12-17 17:11:51 +0700

    ubah

--- if_statement.js
+++ if_statement.js
@@ -4,6 +4,6 @@ console.log("Yeay, aku anak Yuin!")
 
 if (anak_yuin) {
     console.log("Sanaa, sholattt duluu!");
-}
-
-console.log("Aku anak Yuin kok bukan anak Budi");
+} else {
+    console.log("Aku anak Yuin kok bukan anak Budi");
+};
