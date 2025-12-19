798e3bec4542487a68764653b0160e7e0deb3c41
farizwebdev <farizabar80@gmail.com>
2025-12-19 23:51:01 +0700

    ubah

--- for_of.js
+++ for_of.js
@@ -3,3 +3,7 @@ const anak_aku = ['satu', 'dua', 'tiga'];
 for (const anak of anak_aku) {
     console.log(`Aku punya anakk bernama ${anak}`);
 }
+
+for (const angka of [1, 2, 3, 4, 5]) {
+    console.log(`Angka ini adalah angka ${angka}`);
+}
