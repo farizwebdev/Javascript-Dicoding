08656bdb041b563ef97f88eb9f3650feb626d17c
farizwebdev <farizabar80@gmail.com>
2025-12-20 22:09:31 +0700

    ubah

--- for_loop.js
+++ for_loop.js
@@ -2,3 +2,8 @@
 for (pahala = 0; pahala <= 10; pahala++) {
     console.log(`Pahala sholatku ke-${pahala} bertambah`);
 }
+
+// Tentang Menghitung Angka Genap
+for (angka = 0; angka <= 20; angka += 2) {
+    console.log (`Angka Genap: ${angka}`);
+}
